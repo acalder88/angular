e584e5714fa3edce200ca4f621631a25cbe44a24
Alejandro <alejandrocalderonvelez@gmail.com>
2019-07-20 17:59:24 -0500

    Initial structure

---
+++ angularclient/src/app/service/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from '../model/user';
+import { Observable } from 'rxjs/Observable';
+import { environment } from '../../environments/environment';
+ 
+@Injectable()
+export class UserService {
+ 
+  private usersUrl: string;
+ 
+  constructor(private http: HttpClient) {
+    this.usersUrl = environment.domain;
+  }
+ 
+  public findAll(): Observable<User[]> {
+    return this.http.get<User[]>(this.usersUrl);
+  }
+ 
+  public save(user: User) {
+    return this.http.post<User>(this.usersUrl, user);
+  }
+}
