b15637b8d5671328eef9dd628fca967ec27eba02
Alejandro <alejandrocalderonvelez@gmail.com>
2019-12-04 18:38:17 -0500

    Fixed linter

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -3,20 +3,16 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from '../model/user';
 import { Observable } from 'rxjs/Observable';
 import { environment } from '../../environments/environment';
- 
+
 @Injectable()
 export class UserService {
- 
   private usersUrl: string;
- 
   constructor(private http: HttpClient) {
     this.usersUrl = environment.domain;
   }
- 
   public findAll(): Observable<User[]> {
     return this.http.get<User[]>(this.usersUrl);
   }
- 
   public save(user: User) {
     return this.http.post<User>(this.usersUrl, user);
   }
